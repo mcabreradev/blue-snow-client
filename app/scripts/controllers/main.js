02ec4439f0fbdee3105e027ec46e5db819b9d8ce
Miguelangel Cabrera <miguel4ngel@gmail.com>
2014-04-28 22:02:20 -0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('blueSnowApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
